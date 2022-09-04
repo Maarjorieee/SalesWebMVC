7acf6088fd31b06fc3cada47860fcd3881f082c7
Maarjorieee <marjorie_nascimento@hotmail.com>
2022-09-04 19:58:54 -0300

    Remove Group

--- SalesWebMVC/Models/Department.cs
+++ SalesWebMVC/Models/Department.cs
@@ -8,15 +8,13 @@ namespace SalesWebMVC.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-
         public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
 
         public Department()
         {
-            
         }
 
-        public Department (int id, string name)
+        public Department(int id, string name)
         {
             Id = id;
             Name = name;

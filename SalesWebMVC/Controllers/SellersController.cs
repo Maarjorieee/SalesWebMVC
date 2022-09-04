f22f6f62cc21e6c213930fdcb2c2c1cc1442932f
Maarjorieee <marjorie_nascimento@hotmail.com>
2022-09-04 17:22:39 -0300

    Update seller and custom service exception

--- SalesWebMVC/Controllers/SellersController.cs
+++ SalesWebMVC/Controllers/SellersController.cs
@@ -3,6 +3,8 @@ using SalesWebMVC.Models;
 using SalesWebMVC.Services;
 using System.Collections.Generic;
 using SalesWebMVC.Models.ViewModels;
+using System.Security.Cryptography.X509Certificates;
+using SalesWebMVC.Services.Exceptions;
 
 namespace SalesWebMVC.Controllers
 {
@@ -86,9 +88,53 @@ namespace SalesWebMVC.Controllers
             return View(obj);
             }
 
+            
+        }
 
-       }
-        
+        public IActionResult Edit(int? id)
+
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindByID(id.Value);    
+            if(obj == null)
+            {
+                return NotFound();
+            }
+
+            List<Department> departments = _departmentService.FindAll();
+            SellerFormViewModel viewModel = new SellerFormViewModel { Seller = obj, Departments = departments };
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Seller seller)
+        {
+            if(id != seller.Id)
+            {
+                return BadRequest();
+            }
+
+            try
+            {
+                _sellerService.Update(seller);
+                return RedirectToAction(nameof(Index));
+            }
+
+            catch (NotFoundException)
+            {
+                return NotFound();
+            }
+            catch (DbConcurrencyException)
+            {
+                return BadRequest();
+            }
+        }
+
     }
 }
 
